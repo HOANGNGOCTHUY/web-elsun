b2f11bd324794c7ac3c3b7a74c1712686b136b3a
HOANGNGOCTHUY <hoangngocthuy6@gmail.com>
2022-04-14 16:22:40 +0700

    add contact, aboutus

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,3 +21,13 @@ $(document).ready(function(){
         
     });
   });
+
+  // Video
+  var myVideo = document.getElementById("video1");
+
+  function playPause() {
+    if(myVideo.paused)
+      myVideo.play();
+    else
+      myVideo.pause();
+  }
