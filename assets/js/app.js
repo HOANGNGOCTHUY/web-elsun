f99d4dd9965e16a653256a554c2cecd66595f24c
HOANGNGOCTHUY <hoangngocthuy6@gmail.com>
2022-04-29 17:02:53 +0700

    index.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.image-slider').slick({
+    $('.service-image').slick({
         slidesToShow: 4,
         slidesToScroll: 3,
         infinite: true,
@@ -11,16 +11,45 @@ $(document).ready(function(){
   });
 
   $(document).ready(function(){
-    $('.image-customers').slick({
+    $('.customers-infomation').slick({
         slidesToShow: 3,
         slidesToScroll: 3,
         infinite: true,
         arrows: true,
         prevArrow:`<button type='button' class='slick-prev slick-arrow'><i class="icon-left slick-icon"></i></button>`,
-        nextArrow:`<button type='button' class='slick-next slick-arrow'><i class="icon-right-1 slick-icon"></i></button>`
-        
+        nextArrow:`<button type='button' class='slick-next slick-arrow'><i class="icon-right-1 slick-icon"></i></button>`,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows:false,
+              infinite: false,
+          }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
     });
   });
+
 
   // Video
   var myVideo = document.getElementById("video1");
