f5997e5447ad6eb6758adfadde32826d797082fe
HOANGNGOCTHUY <hoangngocthuy6@gmail.com>
2022-05-03 17:10:57 +0700

    commit service

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,17 @@
 $(document).ready(function(){
     $('.service-image').slick({
         slidesToShow: 4,
-        slidesToScroll: 3,
+        slidesToScroll: 4,
         infinite: true,
         arrows: true,
         prevArrow:`<button type='button' class='slick-prev slick-arrow'><i class="icon-left slick-icon"></i></button>`,
-        nextArrow:`<button type='button' class='slick-next slick-arrow'><i class="icon-right-1 slick-icon"></i></button>`
-        
-    });
-  });
-
-  $(document).ready(function(){
-    $('.customers-infomation').slick({
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        arrows: true,
-        prevArrow:`<button type='button' class='slick-prev slick-arrow'><i class="icon-left slick-icon"></i></button>`,
-        nextArrow:`<button type='button' class='slick-next slick-arrow'><i class="icon-right-1 slick-icon"></i></button>`,
+        nextArrow:`<button type='button' class='slick-next slick-arrow'><i class="icon-right-1 slick-icon"></i></button>`,        
         responsive: [
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
+              slidesToShow: 4,
+              slidesToScroll: 4,
               infinite: true,
             }
           },
@@ -39,14 +27,85 @@ $(document).ready(function(){
             settings: {
               slidesToShow: 1,
               slidesToScroll: 1,
-              arrows:false,
-              infinite: false,
+              arrows: false,
+              autoplay: true,
+              dots: true,
+              autoplaySpeed: 1000,
+            }
           }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
         ]
+      });
+    });
+
+  $(document).ready(function(){
+    $('.service-customer').slick({
+        slidesToShow: 7,
+        slidesToScroll: 7,
+        infinite: true,
+        arrows: false,  
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 5,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              arrows: false,
+  
+            }
+          }
+        ]
+      });
+    });
+
+  $(document).ready(function(){
+    $('.customers-infomation').slick({
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      prevArrow:`<button type='button' class='slick-prev slick-arrow'><i class="icon-left slick-icon"></i></button>`,
+      nextArrow:`<button type='button' class='slick-next slick-arrow'><i class="icon-right-1 slick-icon"></i></button>`,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+
+          }
+        }
+      ]
     });
   });
 
